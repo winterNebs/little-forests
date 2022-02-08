e94ec74ff4dd9f45c88437c7652735f47314b7af
Melissa Zhu <17mz1@queensu.ca>
2022-02-08 15:53:39 -0500

    added App.js and components folder

---
+++ client/src/components/common/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navbar } from "../../common";
+import "./Header.css";
+import Button from "@mui/material/Button";
+
+function Header() {
+  return (
+    <section className="header">
+      <section className="header-top">
+        <section className="header-top-logo">
+          <a href="/" className="header-logo">
+            LOGO
+          </a>
+        </section>
+        <section className="header-top-button">
+          <Button variant="contained">Log In</Button>
+        </section>
+      </section>
+
+      <section className="header-bottom">
+        <section className="header-bottom-navbar">{<Navbar />}</section>
+      </section>
+    </section>
+  );
+}
+
+export default Header;
