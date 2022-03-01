0f98eed2ed341180880a60b4fb68df560aad65de
Melissa Zhu <17mz1@queensu.ca>
2022-03-01 13:29:32 -0500

    added some stuff to results list and results page

--- client/src/components/Pages/ForestResultsPage.tsx
+++ client/src/components/Pages/ForestResultsPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ProgressStepper from '../ProgressStepper';
 
 const ForestResultsPage = () => {
   return (
-    <div>ForestResultsPage</div>
+    <div>    
+      <br /><br /><br /><br /><br /><br /><br />
+      <h2>Forest Type</h2>
+      <ProgressStepper />
+      <h2>(Info about forest type)</h2>
+    </div>
   );
 }
 
