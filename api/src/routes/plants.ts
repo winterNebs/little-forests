0fb659cefccab998b1db62db808e57c5c28f0beb
winterNebs <winterNebs@gmail.com>
2022-03-10 13:45:47 -0500

    add another integration example to plant-list

--- api/src/routes/plants.ts
+++ api/src/routes/plants.ts
@@ -4,14 +4,45 @@ import { Plant } from "src/database/schema/plants";
 
 const router: Router = Router();
 //mockup
-const p1: Plant = {commonName:'c1', scientificName:'s1', layer:'tree', treeType:'deciduous', maxHeight:'4', advantages:[],images: []};
-const p2: Plant = {commonName:'c2', scientificName:'s2', layer:'understory tree', treeType:'conifer', maxHeight:'2', advantages:[],images: []};
-const p3: Plant = {commonName:'c3', scientificName:'s3', layer:'canopy', treeType:'evergreen', maxHeight:'0', advantages:[],images: []};
-const p4: Plant = {commonName:'c4', scientificName:'s4', layer:'shrub', treeType:'deciduous', maxHeight:'69', advantages:[],images: []};
-
+const p1: Plant = {
+	commonName: "c1",
+	scientificName: "s1",
+	layer: "tree",
+	treeType: "deciduous",
+	maxHeight: "4",
+	advantages: [],
+	images: [],
+};
+const p2: Plant = {
+	commonName: "c2",
+	scientificName: "s2",
+	layer: "understory tree",
+	treeType: "conifer",
+	maxHeight: "2",
+	advantages: [],
+	images: [],
+};
+const p3: Plant = {
+	commonName: "c3",
+	scientificName: "s3",
+	layer: "canopy",
+	treeType: "evergreen",
+	maxHeight: "0",
+	advantages: [],
+	images: [],
+};
+const p4: Plant = {
+	commonName: "c4",
+	scientificName: "s4",
+	layer: "shrub",
+	treeType: "deciduous",
+	maxHeight: "69",
+	advantages: [],
+	images: [],
+};
 
 router.get("/plants/", (req: Request, res: Response) => {
-	res.send({p1, p2, p3, p4});
+	res.send({ p1, p2, p3, p4 });
 });
 router.get("/plant/:id", (req: Request, res: Response) => {
 	res.send(p1);
@@ -20,7 +51,7 @@ router.get("/plant/:id", (req: Request, res: Response) => {
 router.get(
 	"/api/plants/",
 	(req: Request, res: Response, next: NextFunction) => {
-		res.send([]);
+		res.send({ p1, p2, p3, p4 });
 	}
 );
 
