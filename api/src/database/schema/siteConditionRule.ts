561a4437978c33be7f330ec55245d072a4ece307
winterNebs <winterNebs@gmail.com>
2022-03-14 11:49:11 -0400

    fix build issues

--- api/src/database/schema/siteConditionRule.ts
+++ api/src/database/schema/siteConditionRule.ts
@@ -1,17 +1,19 @@
 import { getModelForClass, prop, ReturnModelType } from "@typegoose/typegoose";
 
-export class SiteConditionRule{
-    @prop()
-    public name!: string;  //name of rule
+export class SiteConditionRule {
+	@prop()
+	public name!: string; //name of rule
 
-    @prop({type: String, required: true, default: []})
-    public allowedValues!: string[];  //values allowed in the rule
+	@prop({ type: String, required: true, default: [] })
+	public allowedValues!: string[]; //values allowed in the rule
 
-    @prop({type: String, required: true, default: []})
-    public images!: string[];
+	@prop({ type: String, required: true, default: [] })
+	public images!: string[];
 }
 
-export const SiteConditionRules:ReturnModelType<
-    typeof SiteConditionRule,
-    {}
-> = getModelForClass(SiteConditionRule, {schemaOptions: {collection: "siteConditionRules"}});
+export const SiteConditionRules: ReturnModelType<
+	typeof SiteConditionRule,
+	{}
+> = getModelForClass(SiteConditionRule, {
+	schemaOptions: { collection: "siteConditionRules" },
+});
